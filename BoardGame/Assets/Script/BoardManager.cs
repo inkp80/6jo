f957f3195b5c473db512538858cc68ee824c27eb
bkd222 <bkd222@naver.com>
2017-10-01 22:10:48 +0900

    added more gamelogic(ex.puttable tiles)

--- BoardGame/Assets/Script/BoardManager.cs
+++ BoardGame/Assets/Script/BoardManager.cs
@@ -23,8 +23,27 @@ public class BoardManager : MonoBehaviour {
 	//추가됨. 8방향을 통틀어 뒤집을 돌 전체를 저장할 리스트
 	private List<Vector3> flipList = new List<Vector3> ();
 
+	//추가됨. 돌을 놓을 수 있는 타일을 저장할 리스트
+	private List<Vector3> availList = new List<Vector3> ();
+
+	//추가됨. 임시 생성된 돌들을 저장할 리스트
+	private List<GameObject> removalList = new List<GameObject>();
+
 	private bool itemActiveState = false;
 	private int itemCode = -1;
+
+	//추가됨. 놓을 곳이 없어서 턴을 넘긴 플레이어를 알려주는 변수
+	private bool whiteTurnSkip = false;
+	private bool blackTurnSkip = false;
+
+	//추가됨. 놓을 수 있는 타일을 한 번만 알려주도록 하는 변수
+	private bool needCreate = true;
+
+	//추가됨. 승자가 결정되는지 알려주는 변수
+	private bool checkWinner = true;
+
+	//추가됨. 놓을 수 있는 자리를 알려주는 임시 프리펩
+	public GameObject removalPrefab;
 
 	public GameObject reversiPrefab;
 	public Piece[,] activedPieces{ set; get; }
@@ -45,8 +64,18 @@ public class BoardManager : MonoBehaviour {
 	private void Update(){
 		UpdateSelection ();
 		DrawBoard ();
+			
+		//추가됨
+		if(checkWinner) {
+			DecideWinner();
+			checkWinner = false;
+		}
 
-
+		//추가됨
+		if (needCreate) {
+			DrawAvail ();
+		}
+			
 		if (Input.GetMouseButtonUp (0)) {
 			if (controllStatus) {
 				Debug.Log ("dont");
@@ -54,9 +83,148 @@ public class BoardManager : MonoBehaviour {
 			}
 			
 			startAction(selectionX, selectionY, currentPlayer);
-		
 		}
 	}
+
+	// 추가됨. 승리조건에 따른 메시지 출력
+	private void DecideWinner() {
+		if (whitePieces == 0) {
+			resultText.text = "Black Wins!";
+			needCreate = false;
+			UIController.result = true;
+		} else if (blackPieces == 0) {
+			resultText.text = "White Wins!";
+			UIController.result = true;
+			needCreate = false;
+		} else if (whiteTurnSkip && blackTurnSkip) {
+			if (whitePieces > blackPieces) {
+				resultText.text = "White Wins!";
+			} else if (blackPieces > whitePieces) {
+				resultText.text = "Black Wins!";
+			} else {
+				resultText.text = "Draw!";
+			}
+			UIController.result = true;
+			needCreate = false;
+		}
+	}
+
+	// 추가됨. 돌을 놓을수 있는 곳을 표시
+	private void DrawAvail() {
+		Vector3 availStone = Vector3.zero;
+
+		availList.Clear ();
+
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if(activedPieces [i, j] != null && activedPieces [i, j].getOwner() == currentPlayer) {
+					checkAvail (i, j);
+				}
+			}
+		}
+
+		if (availList.Count != 0) {
+			if (currentPlayer == 0) {
+				whiteTurnSkip = false;
+			} else {
+				blackTurnSkip = false;
+			}
+
+			while (availList.Count > 0) {
+				availStone.x = availList [0].x;
+				availStone.z = availList [0].z;
+
+				Vector3 spawnPosition = getTileCenter ((int)availStone.x, (int)availStone.z);
+
+				GameObject go = Instantiate (removalPrefab, spawnPosition, Quaternion.identity) as GameObject;
+
+				removalList.Add (go);
+
+				availList.RemoveAt (0);
+			}
+
+			needCreate = false;
+
+		} else {
+			if (currentPlayer == 0) {
+				whiteTurnSkip = true;
+				print ("white가 놓을 곳이 없음. black 턴으로 이동");
+			} else {
+				blackTurnSkip = true;
+				print ("black이 놓을 곳이 없음. white 턴으로 이동");
+			}
+			currentPlayer = ~currentPlayer;
+			needCreate = true;
+			checkWinner = true;
+		}
+	}
+
+	//추가됨. 돌을 놓을 수 있는 타일을 찾는 함수
+	private void checkAvail(int x, int y) {
+		bool enemyFound = false;
+		Vector3 availStone = Vector3.zero;
+
+		int offsetX = 0, offsetY = 0;
+		int tempX = x;
+		int tempY = y;
+		int j = 0;
+
+		for (int i = 0; i < 8; i++) {
+			offsetX = dx [i];
+			offsetY = dy [i];
+
+			tempX += offsetX;
+			tempY += offsetY;
+
+			while (checkArrangeVaildation (tempX, tempY) == true) {
+				if (activedPieces [tempX, tempY] != null && activedPieces [tempX, tempY].getOwner () == ~currentPlayer) {
+					enemyFound = true;
+					tempX += offsetX;
+					tempY += offsetY;
+				} else if (activedPieces [tempX, tempY] == null && enemyFound) {
+					availStone.x = tempX;
+					availStone.z = tempY;
+
+					for (j = 0; j < availList.Count; j++) {
+						if (availList [j] == availStone) {
+							break;
+						}
+					}
+
+					if (j == availList.Count) {
+						availList.Add (availStone);
+					}
+
+					break;
+				} else {
+					break;
+				}
+			}
+
+			tempX = x;
+			tempY = y;
+			enemyFound = false;
+		}
+	}
+
+	//추가됨. 각 사용자의 돌 개수를 세는 함수
+	private void CountPieces() {
+		int white = 0, black = 0;
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (activedPieces [i, j] != null) {
+					if (activedPieces [i, j].getOwner() == 0) {
+						white++;
+					} else {
+						black++;
+					}
+				}
+			}
+		}
+		whitePieces = white;
+		blackPieces = black;
+	}
+
 
 	private void UpdateSelection(){
 		if (!Camera.main) {
@@ -168,6 +336,12 @@ public class BoardManager : MonoBehaviour {
 
 			if (flipList.Count > 0) {
 				controllStatus = true;
+
+				while (removalList.Count > 0) {
+					Destroy (removalList[0]);
+					removalList.RemoveAt (0);
+				}
+
 				spawnPiece (x, y, currentPlayer);
 
 				if (itemActiveState == true) {
@@ -188,14 +362,6 @@ public class BoardManager : MonoBehaviour {
 
 				// 추가됨. 아이템이 선택되지 않았을 경우의 일반적인 경우
 				else {
-					if (currentPlayer == 0) {
-						whitePieces = whitePieces + flipList.Count + 1;
-						blackPieces -= flipList.Count;
-					} else {
-						blackPieces = blackPieces + flipList.Count + 1;
-						whitePieces -= flipList.Count;
-					}
-
 					while (flipList.Count > 0) {
 						nextX = (int)flipList [0].x;
 						nextY = (int)flipList [0].z;
@@ -206,25 +372,10 @@ public class BoardManager : MonoBehaviour {
 					}
 				}
 					
+				CountPieces ();
 				currentPlayer = ~currentPlayer;
-
-				// 추가됨. 승리조건에 따른 메시지 출력
-				if (whitePieces == 0) {
-					resultText.text = "Black Wins!";
-					UIController.result = true;
-				} else if (blackPieces == 0) {
-					resultText.text = "White Wins!";
-					UIController.result = true;
-				} else if (whitePieces + blackPieces == 64) {
-					if (whitePieces > blackPieces) {
-						resultText.text = "White Wins!";
-					} else if (blackPieces > whitePieces) {
-						resultText.text = "Black Wins!";
-					} else {
-						resultText.text = "Draw!";
-					}
-					UIController.result = true;
-				}
+				needCreate = true;
+				checkWinner = true;
 			}
 		}
 	}
@@ -272,6 +423,7 @@ public class BoardManager : MonoBehaviour {
 
 			tempX = selectionX;
 			tempY = selectionY;
+			enemyFound = false;
 		}
 	}
 			
