13c3e32dbbd9efd380d98b9d6cf99179e27e7623
bkd222 <31438580+bkd222@users.noreply.github.com>
2017-11-19 19:19:04 +0900

    Add files via upload
    에러 없도록 각종 코드 수정

---
+++ Capstone/dqn_agent.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 16 01:19:28 2017
+
+@author: bkd
+"""
+
+from collections import deque
+import os
+
+import numpy as np
+import tensorflow as tf
+
+class DQNAgent :
+
+    def __init__ (self, pathName, rows, cols) :
+        tf.reset_default_graph()
+        
+        self.rows = rows
+        self.cols = cols
+        
+        self.name = os.path.splitext(os.path.basename(__file__))[0]
+        
+        self.pathName = pathName
+        
+        self.minibatchSize = 64
+        self.replayMemorySize = 6000
+        self.learningRate = 0.01
+        self.discountFactor = 0.9
+        
+        self.exploration = 0.1
+        
+        self.currentLoss = 0.0
+        
+        # 모델 세이브
+        self.modelDir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "models1")
+        self.modelName = "{}.ckpt".format(self.pathName)
+        
+        # Replay memory
+        self.replayMemory = deque(maxlen = self.replayMemorySize)
+        
+        # 모델 초기화
+        self.initModel()
+
+    def initModel (self, l_rate = 1e-1) :
+        # 2차원 배열을 입력으로 받음
+        self.inputX = tf.placeholder(tf.float32, [None, self.rows, self.cols])
+
+        size =  self.rows *  self.cols
+        X = tf.reshape ( self.inputX, [-1 , size]) # [?,64]
+        
+        # Input Layer
+        W1 = tf.get_variable("W1", shape = [size, size], initializer = tf.contrib.layers.xavier_initializer())        
+        b1 = tf.get_variable("b1", shape = [size], initializer = tf.contrib.layers.xavier_initializer())
+        L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
+        
+        # Hidden Layer
+        W2 = tf.get_variable("W2", shape = [size, size], initializer = tf.contrib.layers.xavier_initializer())        
+        b2 = tf.get_variable("b2", shape = [size], initializer = tf.contrib.layers.xavier_initializer())
+        L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
+
+        # Output Layer
+        W3 = tf.get_variable("W3", shape = [size, size], initializer = tf.contrib.layers.xavier_initializer())        
+        b3 = tf.get_variable("b3", shape = [size], initializer = tf.contrib.layers.xavier_initializer())
+        self.Qpredict = tf.matmul (L2, W3) + b3
+
+        # loss function
+        self.y = tf.placeholder(tf.float32, [None, size])
+        self.loss = tf.reduce_mean(tf.square(self.y - self.Qpredict))
+
+        # train operation
+        self.training = tf.train.AdamOptimizer(learning_rate=l_rate).minimize(self.loss)
+
+        # saver
+        self.saver = tf.train.Saver()
+        
+        # session
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
+
+        # y가 2차원 배열로 리턴되므로 (1,64) [0]번째만 반환 
+    def Qvalues (self, state) :
+        # Q (state, action) of all actions
+        return self.sess.run(self.Qpredict, feed_dict = {self.inputX : [state]})[0]
+
+    def selectAction (self, state, targets, epsilon) :
+        if np.random.rand () <= epsilon :
+            # random
+            randomTargets = []
+            for i in targets :
+                randomTargets.insert(0, i[0] * 8 + i[1])
+            action = np.random.choice(randomTargets)
+            return [action // 8, action % 8]
+        else :
+            # max_action Q (state, action)
+            qvalue, action = self.selectEnableAction(state, targets)
+            return action
+            
+    # qvalue중에서 target안에 있고 가장 큰 값 고름  
+    def selectEnableAction(self, state, targets) :
+        Qs = self.Qvalues(state)
+        # descend = np.sort (Qs)
+        index = np.argsort(Qs) # 값이 제일 작은 것의 index순으로 정렬  
+        for action in reversed(index) :
+            if [action//8, action%8] in targets :
+                break 
+        # max_action Q (state, action)
+        qvalue = Qs[action]
+
+        return qvalue, [action//8, action%8]
+        
+    def storeExperience (self, state, targets, action, reward, state_1, targets_1, terminal) :
+        self.replayMemory.append ((state, targets, action, reward, state_1, targets_1, terminal))
+
+
+
+
+
+    def experienceReplay (self) :
+        state_minibatch = []
+        y_minibatch = []
+
+        # sample random minibatch
+        minibatchSize =  min(len(self.replayMemory), self.minibatchSize)
+        #minibatch size만큼 0~len(D)에 있는 숫자들로 배열 생성 random으로 골라내는것  
+        minibatch_indexes = np.random.randint(0, len(self.replayMemory), minibatchSize)
+
+        for j in minibatch_indexes :
+            state_j, targets_j, action_j, reward_j, state_j_1, targets_j_1, terminal = self.replayMemory[j]
+            action_j_index = action_j[0] * 8 + action_j[1]
+            y_j = self.Qvalues(state_j)
+
+            if terminal :
+                y_j[action_j_index] = reward_j
+            else :
+                # reward_j + gamma * max_action 'Q (state'action ')
+                qvalue, action =  self.selectEnableAction (state_j_1, targets_j_1)
+                y_j [action_j_index] = reward_j + self.discountFactor * qvalue
+
+            state_minibatch.append (state_j)
+            y_minibatch.append (y_j)
+
+        # training
+        self.sess.run(self.training, feed_dict = {self.inputX : state_minibatch, self.y : y_minibatch})
+
+        # for log
+        self.current_loss = self.sess.run(self.loss, feed_dict = { self.inputX : state_minibatch, self.y : y_minibatch})
+
+
+    def loadModel (self, model_path = None) :
+        if model_path :
+            # load from model_path
+            self.saver.restore(self.sess, model_path)
+        else :
+            # load from checkpoint
+            checkpoint = tf.train.get_checkpoint_state(self.modelDir + " ")
+            if checkpoint and checkpoint.model_checkpoint_path :
+                self.saver.restore (self.sess, checkpoint.model_checkpoint_path)
+
+    def saveModel(self) :
+        self.saver.save(self.sess, os.path.join(self.modeDir, self.modelName))
---
+++ Capstone/train.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 19 00:05:50 2017
+
+@author: bkd
+"""
+
+from Reversi import Reversi
+from dqn_agent import DQNAgent
+
+          
+if  __name__  ==  "__main__" :
+    
+    # 반복횟수
+    n_epochs =  1000
+    # Reversi.py 파일의 변수들을 사용하기 위한 env객체를 선언
+    env = Reversi ()
+ 
+    # playerID    
+    playerID = [env.black, env.white, env.black] #black-> white -> black순이므로 
+
+    # player agent. player[0]과 player[1]은 각각 ([0, 1, ... , 63], 'Reversi', 8, 8) 의 값을 가지게 됨
+    players = []
+    # player [0] = env.Black
+    players.append(DQNAgent(env.name, env.rows, env.cols))
+    # player [1] = env.White
+    players.append(DQNAgent(env.name, env.rows, env.cols))
+   
+    for e in  range (n_epochs) :
+        # reset
+        env.reset ()
+        terminal =  False
+        while terminal ==  False : # 1 에피소드가 끝날 때까지 루프
+            
+            for i in  range ( 0 , len (players)) :
+                state = env.screen # 현재 보드판의 상태(배열)을 가져옴
+                targets = env.getEnables (playerID[i]) # 배열(플레이어가 둘 수 있는 위치를 가짐)을 가져옴
+                
+                if  len (targets) >  0 : # 어딘가에 둘 수 있는 장소가있는 경우
+                    # 행동을 선택  
+                    action = players[i].selectAction(state, targets, players[i].exploration)
+                    
+                    # 행동을 실행
+                    env.doFlip(action, playerID[i])
+                        
+                    # 종료 판정
+                    win = env.winner () # 현재 유리한 플레이어
+                    end = env.isEnd ()
+                        
+                    # 다음 상태
+                    state_X = env.screen
+                    target_X = env.getEnables(playerID[i + 1 ])
+                    if  len (target_X) ==  0 :
+                        target_X = env.getEnables(playerID[i])
+                        
+                    # 양자 교육
+                    reward =  0
+                    if end == True :
+                        reward = 1
+                           
+                    players[i].storeExperience (state, targets, action, reward, state_X, target_X, end)
+                    players[i].experienceReplay()                 
+                
+                    # for log
+                    loss = players[i].current_loss
+                    Q_max, Q_action = players[i].selectEnableAction (state, targets)
+                    print("player:{:1d} | pos:[{:d}][{:d}] | LOSS: {:.4f} | Q_MAX: {:.4f}".format(playerID[i], action[0], action[1], loss, Q_max))
+
+                # 행동을 실행 한 결과
+                terminal = env.isEnd()     
+
+        w = env.winner ()
+        print("EPOCH: {:03d}/{:03d} | WIN: player{:1d}".format(e, n_epochs, w))                  
+        
+    # 저장後攻의 player2를 저장한다.
+    players [ 1 ] .save_model ()    
